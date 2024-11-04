c70267b24ecd96f74ce5bab274207565ab587832
Gianmaria Longobucco <gmlongobucco@gmail.com>
2024-11-04 15:40:59 +0100

    Initial Commit

---
+++ OnBeat/OnBeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBeatApp.swift
+//  OnBeat
+//
+//  Created by Gianmaria Longobucco on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct OnBeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
